514ee98709f389bb84fe0480a58f351c650da1b0
Sudhanshu Kumar <122782163+sudhanshukumar1729@users.noreply.github.com>
2023-12-07 00:04:49 +0530

    Update fragment3.java

--- fragment3.java
+++ fragment3.java
@@ -14,6 +14,8 @@ import androidx.fragment.app.Fragment;
 public class fragment3 extends Fragment {
 
     public interface OnSubmitClickListener {
+        void onCreate(Bundle savedInstanceState3);
+
         void onSubmitClicked3(int calorie);
     }
 
@@ -40,7 +42,7 @@ public class fragment3 extends Fragment {
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view2) {
                 calculateAndSubmit3();
             }
         });

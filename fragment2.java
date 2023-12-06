2f8752e5d331ed2f4dab8978df470cf669b11b0f
Sudhanshu Kumar <122782163+sudhanshukumar1729@users.noreply.github.com>
2023-12-07 00:04:26 +0530

    Update fragment2.java

--- fragment2.java
+++ fragment2.java
@@ -14,6 +14,8 @@ import androidx.fragment.app.Fragment;
 public class fragment2 extends Fragment {
 
     public interface OnSubmitClickListener {
+        void onCreate(Bundle savedInstanceState2);
+
         void onSubmitClicked2(int calorie);
     }
 
@@ -40,7 +42,7 @@ public class fragment2 extends Fragment {
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view2) {
                 calculateAndSubmit2();
             }
         });
